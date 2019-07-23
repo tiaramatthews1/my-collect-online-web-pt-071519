edd14209e16758da581b5e3abcd2b786829b578d
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-23 16:21:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,3 @@
-def my_collect(x)
-x = 0
-  collection = []
-  while x < array.length
-    collection << 
-    yield(array[x])
-    x += 1
-  end
-  collection
-end
-
 def my_collect(languages)
 collection = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(collection) do |lang|
