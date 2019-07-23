00ca9b2329baa2285432bf93f4c81624e342ce75
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-23 16:21:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,7 +9,7 @@ x = 0
   collection
 end
 
-def my_collect(array)
+def my_collect(languages)
 collection = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(collection) do |lang|
   lang.upcase
